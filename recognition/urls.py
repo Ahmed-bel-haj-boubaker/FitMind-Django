3e3dc0d07a82b7f8e168b70082d8a270dde12938
Ahmed bel haj boubaker <Ahmed.belhajboubaker@esprit.tn>
2024-10-28 13:34:37 +0100

    ..

--- recognition/urls.py
+++ recognition/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-# from .views import upload_image
+
 from .views import upload_video
 
 urlpatterns = [
-# path('upload/', upload_image, name='upload'),
-path('upload/', upload_video, name='upload_video'),
-
+    # path('upload/', upload_image, name='upload'),
+    path("upload/", upload_video, name="upload_video"),
 ]
